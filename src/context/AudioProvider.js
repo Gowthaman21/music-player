5650220cc1b9eaf9dd68e2510398e5637e91086a
Gowthaman21 <manogowtham211@gmail.com>
2021-11-21 10:42:33 +0530

    united states

--- src/context/AudioProvider.js
+++ src/context/AudioProvider.js
@@ -4,105 +4,63 @@ import { Audio } from "expo-av";
 export const AudioContext = createContext();
 
 const AudioProvider = ({ children }) => {
-    const [audioFiles, setaudioFiles] = useState([]);
     const [playList, setplayList] = useState([]);
     const [addToPlayList, setaddToPlayList] = useState(null);
-    const [playbackObj, setplaybackObj] = useState(null);
-    const [soundObj, setsoundObj] = useState(null);
-    const [currentAudio, setcurrentAudio] = useState({});
-    const [isPlaying, setisPlaying] = useState(false);
     const [isPlayListRunning, setisPlayListRunning] = useState(false);
     const [activePlayList, setactivePlayList] = useState([]);
-    const [currentAudioIndex, setcurrentAudioIndex] = useState("");
-    const [playbackPosition, setplaybackPosition] = useState(null);
-    const [playbackDuration, setplaybackDuration] = useState(null);
-    const [totalAudioCount, settotalAudioCount] = useState(0);
 
-    const toggleisPlaying = () => setisPlaying(!isPlaying);
+    const [details, setDetails] = useState({
+        audioFiles: [],
+        playbackObj: null,
+        soundObj: null,
+        currentAudio: {},
+        isPlaying: false,
+        currentAudioIndex: "",
+        playbackPosition: null,
+        playbackDuration: null,
+        totalAudioCount: 0,
+    });
+
+    const handleChange = (newState) => {
+        setDetails((prevState) => ({
+            ...prevState,
+            ...newState,
+        }));
+    };
     useEffect(() => {
         getFiles();
     }, []);
 
     const onPlaybackStatusUpdate = async (playbackStatus) => {
         if (playbackStatus.isLoaded && playbackStatus.isPlaying) {
-            setplaybackPosition(playbackStatus.positionMillis);
-            setplaybackDuration(playbackStatus.durationMillis);
+            handleChange({
+                playbackPosition: playbackStatus.positionMillis,
+                playbackDuration: playbackStatus.durationMillis,
+            });
+            // setplaybackPosition(playbackStatus.positionMillis);
+            // setplaybackDuration(playbackStatus.durationMillis);
         }
-
-        // if (playbackStatus.isLoaded && !playbackStatus.isPlaying) {
-        //   storeAudioForNextOpening(
-        //     this.state.currentAudio,
-        //     this.state.currentAudioIndex,
-        //     playbackStatus.positionMillis
-        //   );
-        // }
-
-        // if (playbackStatus.didJustFinish) {
-        //   if (this.state.isPlayListRunning) {
-        //     let audio;
-        //     const indexOnPlayList = this.state.activePlayList.audios.findIndex(
-        //       ({ id }) => id === this.state.currentAudio.id
-        //     );
-        //     const nextIndex = indexOnPlayList + 1;
-        //     audio = this.state.activePlayList.audios[nextIndex];
-
-        //     if (!audio) audio = this.state.activePlayList.audios[0];
-
-        //     const indexOnAllList = this.state.audioFiles.findIndex(
-        //       ({ id }) => id === audio.id
-        //     );
-
-        //     const status = await playNext(this.state.playbackObj, audio.uri);
-        //     return this.updateState(this, {
-        //       soundObj: status,
-        //       isPlaying: true,
-        //       currentAudio: audio,
-        //       currentAudioIndex: indexOnAllList,
-        //     });
-        //   }
-
-        //   const nextAudioIndex = this.state.currentAudioIndex + 1;
-        //   // there is no next audio to play or the current audio is the last
-        //   if (nextAudioIndex >= this.totalAudioCount) {
-        //     this.state.playbackObj.unloadAsync();
-        //     this.updateState(this, {
-        //       soundObj: null,
-        //       currentAudio: this.state.audioFiles[0],
-        //       isPlaying: false,
-        //       currentAudioIndex: 0,
-        //       playbackPosition: null,
-        //       playbackDuration: null,
-        //     });
-        //     return await storeAudioForNextOpening(this.state.audioFiles[0], 0);
-        //   }
-        //   // otherwise we want to select the next audio
-        //   const audio = this.state.audioFiles[nextAudioIndex];
-        //   const status = await playNext(this.state.playbackObj, audio.uri);
-        //   this.updateState(this, {
-        //     soundObj: status,
-        //     currentAudio: audio,
-        //     isPlaying: true,
-        //     currentAudioIndex: nextAudioIndex,
-        //   });
-        //   await storeAudioForNextOpening(audio, nextAudioIndex);
-        // }
     };
 
     const play = async (uri, lastPosition) => {
         try {
+            console.log("uri", uri);
+
             if (!lastPosition)
-                return await playbackObj.loadAsync(
+                return await details.playbackObj.loadAsync(
                     { uri },
                     { shouldPlay: true, progressUpdateIntervalMillis: 1000 }
                 );
 
             // but if there is lastPosition then we will play audio from the lastPosition
-            await playbackObj.loadAsync(
+            await details.playbackObj.loadAsync(
                 { uri },
                 { progressUpdateIntervalMillis: 1000 }
             );
 
-            return await playbackObj.playFromPositionAsync(lastPosition);
+            return await details.playbackObj.playFromPositionAsync(
+                lastPosition
+            );
         } catch (error) {
             console.log("error inside play helper method", error.message);
         }
@@ -110,7 +68,7 @@ const AudioProvider = ({ children }) => {
 
     const pause = async () => {
         try {
-            return await playbackObj.setStatusAsync({
+            return await details.playbackObj.setStatusAsync({
                 shouldPlay: false,
             });
         } catch (error) {
@@ -120,7 +78,9 @@ const AudioProvider = ({ children }) => {
 
     const resume = async () => {
         try {
-            return await playbackObj.setStatusAsync({ shouldPlay: true });
+            return await details.playbackObj.setStatusAsync({
+                shouldPlay: true,
+            });
         } catch (error) {
             console.log("error inside resume helper method", error.message);
         }
@@ -128,8 +88,8 @@ const AudioProvider = ({ children }) => {
 
     const playNext = async (uri) => {
         try {
-            await playbackObj.stopAsync();
-            await playbackObj.unloadAsync();
+            await details.playbackObj.stopAsync();
+            await details.playbackObj.unloadAsync();
             return await play(uri);
         } catch (error) {
             console.log("error inside playNext helper method", error.message);
@@ -138,49 +98,84 @@ const AudioProvider = ({ children }) => {
 
     const selectAudio = async (key) => {
         try {
-            let det = audioFiles.find((i) => i.key === key.toString());
+            console.log("key", key);
 
-            if (soundObj === null) {
+            let det = details.audioFiles.find((i) => i.key === key.toString());
+            console.log("song", det.audioUrl);
+
+            if (details.soundObj === null) {
                 // playing for first time
+                console.log("hhhh", det.audioUrl);
                 const status = await play(det.audioUrl);
-                setcurrentAudio(det);
-                setcurrentAudioIndex(det.key);
-                setsoundObj(status);
-                setisPlaying(true);
-                playbackObj.setOnPlaybackStatusUpdate(onPlaybackStatusUpdate);
+
+                // setcurrentAudio(det);
+                // setcurrentAudioIndex(det.key);
+                // setsoundObj(status);
+                // setisPlaying(true);
+                handleChange({
+                    currentAudio: det,
+                    currentAudioIndex: det.key,
+                    soundObj: status,
+                    isPlaying: true,
+                });
+                details.playbackObj.setOnPlaybackStatusUpdate(
+                    onPlaybackStatusUpdate
+                );
+                return;
             }
 
             if (
                 //pause Audio
-                soundObj.isLoaded &&
-                soundObj.isPlaying &&
-                currentAudioIndex === det.key
+                details.soundObj.isLoaded &&
+                details.soundObj.isPlaying &&
+                details.currentAudioIndex === det.key
             ) {
                 const status = await pause();
-                setsoundObj(status);
-                setisPlaying(false);
-                setplaybackPosition(status.positionMillis);
+                // setsoundObj(status);
+                // setisPlaying(false);
+                // setplaybackPosition(status.positionMillis);
+                handleChange({
+                    currentAudio: det,
+                    currentAudioIndex: det.key,
+                    soundObj: status,
+                    isPlaying: false,
+                });
+                return;
             }
 
             if (
-                soundObj.isLoaded &&
-                !soundObj.isPlaying &&
-                currentAudioIndex === det.key
+                details.soundObj.isLoaded &&
+                !details.soundObj.isPlaying &&
+                details.currentAudioIndex === det.key
             ) {
                 const status = await resume();
-                setsoundObj(status);
-                setisPlaying(true);
+                // setsoundObj(status);
+                // setisPlaying(true);
+                handleChange({
+                    soundObj: status,
+                    isPlaying: true,
+                });
+                return;
             }
 
-            if (soundObj.isLoaded && currentAudioIndex !== det.key) {
+            if (
+                details.soundObj.isLoaded &&
+                details.currentAudioIndex !== det.key
+            ) {
                 //select another file
                 const status = await playNext(det.audioUrl);
                 console.log("status", status);
-
-                setcurrentAudio(det);
-                setcurrentAudioIndex(det.key);
-                setsoundObj(status);
-                setisPlaying(true);
+                handleChange({
+                    currentAudio: det,
+                    currentAudioIndex: det.key,
+                    soundObj: status,
+                    isPlaying: true,
+                });
+                return;
+                // setcurrentAudio(det);
+                // setcurrentAudioIndex(det.key);
+                // setsoundObj(status);
+                // setisPlaying(true);
             }
         } catch (error) {
             console.log("error inside select audio method.", error.message);
@@ -190,70 +185,49 @@ const AudioProvider = ({ children }) => {
     const getFiles = async () => {
         let val = await getPermission();
         if (val.done === true) {
-            setplaybackObj(new Audio.Sound());
-            setaudioFiles(val.final);
+            // setplaybackObj(new Audio.Sound());
+            // setaudioFiles(val.final);
+            handleChange({
+                playbackObj: new Audio.Sound(),
+                audioFiles: val.final,
+            });
         }
         // setaudioFiles(await getPermission());
     };
 
     const changeAudio = async (select) => {
-        const { isLoaded } = await playbackObj.getStatusAsync();
+        const { isLoaded } = await details.playbackObj.getStatusAsync();
         const isLastAudio =
-            (Number(currentAudioIndex) + 1).toString() === totalAudioCount;
-        const isFirstAudio = currentAudioIndex <= 10;
+            (Number(details.currentAudioIndex) + 1).toString() ===
+            details.totalAudioCount;
+        const isFirstAudio = details.currentAudioIndex <= 10;
 
         let audio;
         let index;
         let status;
         try {
             if (select === "next") {
-                audio = audioFiles.find(
-                    (i) => i.key === (Number(currentAudioIndex) + 1).toString()
+                audio = details.audioFiles.find(
+                    (i) =>
+                        i.key ===
+                        (Number(details.currentAudioIndex) + 1).toString()
                 );
                 if (!isLoaded && !isLastAudio) {
-                    index = (Number(currentAudioIndex) + 1).toString();
+                    index = (Number(details.currentAudioIndex) + 1).toString();
                     status = await play(audio.audioUrl);
-                    playbackObj.setOnPlaybackStatusUpdate(
-                        onPlaybackStatusUpdate
+                    details.playbackObj.setOnPlaybackStatusUpdate(
+                        details.onPlaybackStatusUpdate
                     );
                 }
 
                 if (isLoaded && !isLastAudio) {
-                    index = (Number(currentAudioIndex) + 1).toString();
+                    index = (Number(details.currentAudioIndex) + 1).toString();
                     status = await playNext(audio.audioUrl);
                 }
 
                 if (isLastAudio) {
                     index = 0;
-                    audio = audioFiles[index];
-                    if (isLoaded) {
-                        status = await playNext(playbackObj, audio.audioUrl);
-                    } else {
-                        status = await play(playbackObj, audio.audioUrl);
-                    }
-                }
-            }
-            if (select === "prev") {
-                audio = audioFiles.find(
-                    (i) => i.key === (Number(currentAudioIndex) - 1).toString()
-                );
-
-                if (!isLoaded && !isFirstAudio) {
-                    index = currentAudioIndex - 1;
-                    status = await play(audio.audioUrl);
-                    playbackObj.setOnPlaybackStatusUpdate(
-                        onPlaybackStatusUpdate
-                    );
-                }
-
-                if (isLoaded && !isFirstAudio) {
-                    index = currentAudioIndex - 1;
-                    status = await playNext(audio.audioUrl);
-                }
-
-                if (isFirstAudio) {
-                    index = totalAudioCount - 1;
-                    audio = audioFiles[index];
+                    audio = details.audioFiles[index];
                     if (isLoaded) {
                         status = await playNext(audio.audioUrl);
                     } else {
@@ -261,27 +235,68 @@ const AudioProvider = ({ children }) => {
                     }
                 }
             }
-            // write code for prev and next song
-            setcurrentAudio(audio);
-            setsoundObj(status);
-            setisPlaying(true);
-            setcurrentAudioIndex(index);
-            setplaybackPosition(null);
-            setplaybackDuration(null);
+            if (select === "prev") {
+                audio = details.audioFiles.find(
+                    (i) =>
+                        i.key ===
+                        (Number(details.currentAudioIndex) - 1).toString()
+                );
+
+                if (!isLoaded && !isFirstAudio) {
+                    index = details.currentAudioIndex - 1;
+                    status = await play(audio.audioUrl);
+                    details.playbackObj.setOnPlaybackStatusUpdate(
+                        onPlaybackStatusUpdate
+                    );
+                }
+
+                if (isLoaded && !isFirstAudio) {
+                    index = details.currentAudioIndex - 1;
+                    status = await playNext(audio.audioUrl);
+                }
+
+                if (isFirstAudio) {
+                    index = details.totalAudioCount - 1;
+                    audio = details.audioFiles[index];
+                    if (isLoaded) {
+                        status = await playNext(audio.audioUrl);
+                    } else {
+                        status = await play(audio.audioUrl);
+                    }
+                }
+            }
+            handleChange({
+                currentAudio: audio,
+                soundObj: status,
+                isPlaying: true,
+                currentAudioIndex: index,
+                playbackPosition: null,
+                playbackDuration: null,
+            });
+            // setcurrentAudio(audio);
+            // setsoundObj(status);
+            // setisPlaying(true);
+            // setcurrentAudioIndex(index);
+            // setplaybackPosition(null);
+            // setplaybackDuration(null);
         } catch (error) {
             console.log("error inside change audio method.", error.message);
         }
     };
 
     const moveAudio = async (value) => {
-        if (soundObj === null || !isPlaying) return;
+        if (details.soundObj === null || !details.isPlaying) return;
 
         try {
-            const status = await playbackObj.setPositionAsync(
-                Math.floor(soundObj.durationMillis * value)
+            const status = await details.playbackObj.setPositionAsync(
+                Math.floor(details.soundObj.durationMillis * value)
             );
-            setsoundObj(status);
-            setplaybackPosition(status.positionMillis);
+            // setsoundObj(status);
+            // setplaybackPosition(status.positionMillis);
+            handleChange({
+                soundObj: status,
+                playbackPosition: status.positionMillis,
+            });
 
             await resume();
         } catch (error) {
@@ -290,34 +305,26 @@ const AudioProvider = ({ children }) => {
     };
 
     useEffect(() => {
-        settotalAudioCount(audioFiles.length);
-    }, [audioFiles]);
+        // settotalAudioCount(audioFiles.length);
+        // details.audioFiles.map((i) => {
+        //     console.log("file", i.key, i.title, i.audioUrl);
+        // });
+        handleChange({ totalAudioCount: details.audioFiles.length });
+    }, [details.audioFiles]);
 
     return (
         <AudioContext.Provider
             value={{
-                audioFiles,
                 playList,
                 addToPlayList,
-                playbackObj,
-                soundObj,
-                currentAudio,
-                isPlaying,
-                currentAudioIndex,
-                // totalAudioCount: this.totalAudioCount,
-                playbackPosition,
-                playbackDuration,
                 isPlayListRunning,
                 activePlayList,
-                toggleisPlaying,
+                details,
                 selectAudio,
                 changeAudio,
                 pause,
                 moveAudio,
                 onPlaybackStatusUpdate,
-                // updateState: this.updateState,
-                // loadPreviousAudio: this.loadPreviousAudio,
-                // onPlaybackStatusUpdate: this.onPlaybackStatusUpdate,
             }}
         >
             {children}
