ed5f81a3aeb1b12805d484c3506543cc775c5acf
Gowthaman21 <manogowtham211@gmail.com>
2021-11-21 10:41:14 +0530

    improved code

--- src/api/GetPermission.js
+++ src/api/GetPermission.js
@@ -1,8 +1,8 @@
 import * as MediaLibrary from "expo-media-library";
 import MusicInfo from "expo-music-info";
 
-const readProp = async (url) => {
-    let data = await MusicInfo.getMusicInfoAsync(url, {
+const readProp = async (i) => {
+    let data = await MusicInfo.getMusicInfoAsync(i.url, {
         title: true,
         artist: true,
         album: true,
@@ -10,7 +10,9 @@ const readProp = async (url) => {
         picture: true,
     });
 
-    return data;
+    let det = { ...i, ...data };
+
+    return det;
 };
 
 function fmtMSS(s) {
@@ -49,49 +51,55 @@ export const getPermission = async () => {
     if (permission.granted) {
         let dum = await getAudioFiles();
         let dum2 = [];
-        const requests = dum.map(async (i) => dum2.push(await readProp(i.uri)));
-        await Promise.all(requests).then(() => {
-            dum2.map((i) => {
-                // console.log("log", i.album);
-            });
-        });
+        const requests = dum.map(async (i) => dum2.push(await readProp(i)));
+        // await Promise.all(requests).then(() => {
+        //     dum2.map((i) => {
+        //         console.log("log", i.album);
+        //     });
+        // });
         let final = [];
-        if (dum.length === dum2.length) {
-            for (var i = 0; i < dum.length; i++) {
-                let d1 = dum[i];
-                let d2 = dum2[i]; //make default values
-                if (d2 === null) {
-                    d2 = {
-                        title: d1.filename,
-                        album: "unknown album",
-                        artist: "unknown",
-                        picture: {
-                            pictureData:
-                                "https://st.depositphotos.com/3538103/5169/i/950/depositphotos_51692599-stock-photo-music-icon-design.jpg",
-                        },
-                    };
-                }
-                let g = d2?.picture?.pictureData;
-                if (!g) {
-                    d2.picture = {
+        for (var i = 0; i < dum2.length; i++) {
+            let d2 = dum2[i]; //make default values
+            if (d2 === null) {
+                d2 = {
+                    title: d1.filename,
+                    album: "unknown album",
+                    artist: "unknown",
+                    picture: {
                         pictureData:
                             "https://st.depositphotos.com/3538103/5169/i/950/depositphotos_51692599-stock-photo-music-icon-design.jpg",
-                    };
-                }
-
-                let temp = {
-                    key: (i + 10).toString(),
-                    title: d2.title,
-                    audioUrl: d1.uri,
-                    artist: d2.artist,
-                    album: d2.album,
-                    duration: fmtMSS(d1.duration),
-                    albumArtUrl: d2?.picture?.pictureData,
+                    },
                 };
-                // console.log("temp", temp.artist);
-
-                final.push(temp);
             }
+            if (!d2.title) {
+                d2.title = d2.filename;
+            }
+            if (!d2.album) {
+                d2.album = "unknown album";
+            }
+            if (!d2.artist) {
+                d2.artist = "unknown";
+            }
+            let g = d2?.picture?.pictureData;
+            if (!g || g === "data:image/jpeg;base64,") {
+                d2.picture = {
+                    pictureData:
+                        "https://st.depositphotos.com/3538103/5169/i/950/depositphotos_51692599-stock-photo-music-icon-design.jpg",
+                };
+            }
+
+            let temp = {
+                key: (i + 10).toString(),
+                title: d2.title,
+                audioUrl: d2.uri,
+                artist: d2.artist,
+                album: d2.album,
+                duration: fmtMSS(d2.duration),
+                albumArtUrl: d2?.picture?.pictureData,
+            };
+            // console.log("temp", temp.artist);
+
+            final.push(temp);
         }
 
         return { final: final, done: true };
